71100f47bd7d4bce15fddb048ebcd419227ca0fd
Russell Emerine <rre0520@gmail.com>
2023-10-30 14:36:47 -0400

    Rebased the project on walkmesh game5, rather than multiplayer game6

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -1,7 +1,7 @@
 #include "Mode.hpp"
 
-#include "Connection.hpp"
-#include "Game.hpp"
+#include "Scene.hpp"
+#include "WalkMesh.hpp"
 
 #include <glm/glm.hpp>
 
@@ -9,26 +9,34 @@
 #include <deque>
 
 struct PlayMode : Mode {
-	PlayMode(Client &client);
-	virtual ~PlayMode();
-
-	//functions called by main loop:
-	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
-	virtual void update(float elapsed) override;
-	virtual void draw(glm::uvec2 const &drawable_size) override;
-
-	//----- game state -----
-
-	//input tracking for local player:
-	Player::Controls controls;
-
-	//latest game state (from server):
-	Game game;
-
-	//last message from server:
-	std::string server_message;
-
-	//connection to server:
-	Client &client;
-
+    PlayMode();
+    
+    ~PlayMode() override;
+    
+    //functions called by main loop:
+    bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
+    
+    void update(float elapsed) override;
+    
+    void draw(glm::uvec2 const &drawable_size) override;
+    
+    //----- game state -----
+    
+    //input tracking:
+    struct Button {
+        uint8_t downs = 0;
+        uint8_t pressed = 0;
+    } left, right, down, up;
+    
+    //local copy of the game scene (so code can change it during gameplay):
+    Scene scene;
+    
+    //player info:
+    struct Player {
+        WalkPoint at;
+        //transform is at player's feet and will be yawed by mouse left/right motion:
+        Scene::Transform *transform = nullptr;
+        //camera is at player's head and will be pitched by mouse up/down motion:
+        Scene::Camera *camera = nullptr;
+    } player;
 };
